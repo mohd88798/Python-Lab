05fd68effbc9a6eac0f6ced43f90164f95bd5811
mohd88798 <mohdzaid88798@gmail.com>
2022-04-10 23:27:32 +0530

    1.4

---
+++ Basic/Divisible.py
@@ -0,0 +1,8 @@
+# Print all the numbers between 100 and 200 divisible by 4 and 7
+
+print("Numbers divisible by 4 are: ",end='')
+for i in range(100,201,4):
+    print(i,end=' ')
+print("\nNumbers divisible by 7 are: ",end='')
+for i in range(100,201,7):
+    print(i,end=' ')
